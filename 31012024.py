76fd13f0e8b20dfbae6b0a517ae0a6c4aecbb035
Sachin Bisht <sachin.bisht@neo-world.com>
2024-01-31 22:57:16 +0530

    31st jan

---
+++ 31012024.py
@@ -0,0 +1,17 @@
+# Problem Link : https://www.geeksforgeeks.org/problems/trie-insert-and-search0651/1
+class Solution:
+    def insert(self, root, key):
+        curr_node = root
+        for c in key:
+            if curr_node.children[ord(c) - ord("a")] == None:
+                new_node = TrieNode()
+                curr_node.children[ord(c) - ord("a")] = new_node
+            curr_node = curr_node.children[ord(c) - ord("a")]
+        curr_node.isEndOfWord = True
+    def search(self, root, key):
+        curr_node = root
+        for c in key:
+            if curr_node.children[ord(c) - ord("a")] == None:
+                return False
+            curr_node = curr_node.children[ord(c) - ord("a")]
+        return curr_node.isEndOfWord
