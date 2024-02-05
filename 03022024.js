4a6bb784778bc83058b47f72ab48a83ac0761a53
Sachin Bisht <bishtman12@gmail.com>
2024-02-05 20:38:56 +0530

    5th feb

--- 03022024.js
+++ 03022024.js
@@ -3,30 +3,30 @@ class Node {
         this.data = x;
         this.next = null;
     }
-    decimalValue(head){
+    decimalValue(head) {
         let new_head = this.reverseLinkedList(head);
-        const MOD = 10**9+7;
+        const MOD = 10 ** 9 + 7;
         let answer = 0;
         let i = 0;
-        
+
         while (new_head) {
-            if (i == 0 && new_head.data == 0){
+            if (i == 0 && new_head.data == 0) {
                 new_head = new_head.next
                 i += 1
                 continue
             }
             console.log(new_head.data)
             let num = 1;
-            new_head.data == 1 ? num = 2 : num  = 1;
+            new_head.data == 1 ? num = 2 : num = 1;
 
             answer = answer + num ** i
             new_head = new_head.next
             i += 1
         }
         console.log(answer)
-        return answer%MOD
-      
-  }
+        return answer % MOD
+
+    }
 
     reverseLinkedList(head) {
         let temp = null;
@@ -41,26 +41,54 @@ class Node {
         return prev
     }
 
-    printList(head){
+    printList(head) {
         let new_head = this.reverseLinkedList(head);
         console.log(new_head)
-        while (new_head){
+        while (new_head) {
             console.log(new_head.data);
             new_head = new_head.next
         }
     }
+
+    sortedInsert(head, data) {
+        const insert_node = new Node(data);
+        if (data <= head.data) {
+            insert_node.next = head;
+            return insert_node
+        }
+        const store_head = head;
+        let prev = new Node(null);
+        while (head) {
+
+            if (data <= head.data || head.data < prev.data) {
+                prev.next = insert_node;
+                insert_node.next = head;
+                break;
+            }
+            prev = head;
+            head = head.next
+        }
+        console.log("STORE HEAD",store_head)
+        this.printList(store_head);
+        return store_head
+    }
 }
 
-const A = new Node("0");
-const B = new Node("1");
-const C = new Node("1");
-const D = new Node("0");
+const A = new Node("1");
+const B = new Node("3");
+const C = new Node("4");
+const D = new Node("5");
 A.next = B;
 
 B.next = C
 
+C.next = D
+
+D.next = A
+
 // C.next = D
 
 // A.printList(A); // Invoking the printList function
-A.decimalValue(A)
-console.log(2**0)
+// A.decimalValue(A)
+// console.log(2 ** 0)
+A.sortedInsert(A,6)
--- 05022024.js
+++ 05022024.js
@@ -1,1 +1,33 @@
-// sample commit
+// Problem Link : https://www.geeksforgeeks.org/problems/sorted-insert-for-circular-linked-list/1
+
+class Solution {
+    
+    sortedInsert(head, data)
+    {
+        const insert_node = new Node(data);
+        const store_head = head;
+        let prev = new Node(null);
+        
+        if(head === null){
+            insert_node.next = insert_node;
+            return insert_node
+        }
+        // first element of the list
+        if(data<= head.data){
+            insert_node.next = head;
+            while (head.next != store_head){
+                head = head.next
+            }
+            head.next = insert_node;
+            return insert_node
+        }
+        
+        while (head.next != store_head && head.next.data <= data) {
+            head = head.next
+        }
+        insert_node.next = head.next;
+        head.next = insert_node;
+        return store_head
+    }
+    
+}
