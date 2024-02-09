cd49ce83e4fa8cc4984fb3757a2eb7a5b705400c
Sachin Bisht <bishtman12@gmail.com>
2024-02-09 23:37:11 +0530

    9th feb

---
+++ 09022024.py
@@ -0,0 +1,17 @@
+# Problem Link https://www.geeksforgeeks.org/problems/children-sum-parent/1
+class Solution:
+    def isSumProperty(self, root):
+        # code here
+        if root:
+            if not root.left and not root.right:
+                return 1
+            s = root.left.data if root.left else 0
+            s+= root.right.data if root.right else 0
+            
+            if s!=root.data:
+                return 0
+            if self.isSumProperty(root.left) and self.isSumProperty(root.right):
+                return 1
+            else:
+                return 0
+        return 1
